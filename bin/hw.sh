07728d6cb9fe9f0317689b70193e192c417d4433
Denis Fortict <d.fortict@vivit.fr>
2020-12-27 19:43:35 +0100

    Hello World.sh genere avec template vim

---
+++ bin/hw.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#: Title          : hw.sh
+#: Date           : 2020 Dec 27
+#: Author         : Denis Fortict
+#: Version        : 1.0
+#: Description    : Hello, World!
+#: Options        : None
+#: Last modified  : 2020 Dec 27 19 40 06
+
+printf "%s\n" "Hello, World!"
+
