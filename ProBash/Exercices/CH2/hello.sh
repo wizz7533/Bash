4531896013fcd60c3e9b146d3631664dde770b97
Denis Fortict <d.fortict@vivit.fr>
2020-12-28 02:52:44 +0100

    print Hello and the first command-line argument

---
+++ ProBash/Exercices/CH2/hello.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#: Title          : hello.sh
+#: Date           : 2020 Dec 28
+#: Author         : Denis Fortict
+#: Version        : 1.0
+#: Description    : print Hello and the first command-line argument
+#: Options        : none
+#: Last modified  : 2020 Dec 28 02 43 21
+
+printf "Hello, %s!\n" "$1"
+
